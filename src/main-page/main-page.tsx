bfbc4885c496e3366f381fe20abeba29ff591eb8
VinkiLilai <anastasia.gorodnichaya@gmail.com>
2024-12-07 17:31:06 +0500

    Sorting

--- src/main-page/main-page.tsx
+++ src/main-page/main-page.tsx
@@ -7,9 +7,11 @@ import {useAppSelector} from '../hooks';
 import {Point} from '../types/point.ts';
 import {setActiveCity} from '../store/action.ts';
 import {CitiesList} from '../cities-list/cities-list.tsx';
-import {Cities} from '../const.tsx';
+import {Cities, SORTING_MODE} from '../const.tsx';
 import {CitiesMock} from '../mocks/cities.ts';
 import {City} from '../types/city.ts';
+import {SortingMode} from '../types/sorting-mode.ts';
+import {SortingModes} from '../sorting-modes/sorting-modes.tsx';
 
 function MainPage(): JSX.Element {
   function getPlacesText(count: number): string {
@@ -21,11 +23,24 @@ function MainPage(): JSX.Element {
   }
 
   const [selectedOfferId, setSelectedOfferId] = useState<Nullable<string>>();
+  const [sortingMode, setSortingMode] = useState<SortingMode>(SORTING_MODE.Popular);
   const dispatch = useDispatch();
   const activeCity = useAppSelector((state) => state.activeCity);
   const activeOffers = useAppSelector((state) => state.offers);
 
   const filteredOffers = activeOffers.filter((offer) => offer.city.name === activeCity.name);
+  const sortedOffers = filteredOffers.sort((first, second) => {
+    switch (sortingMode) {
+      case 'Price: high to low':
+        return second.price - first.price;
+      case 'Price: low to high':
+        return first.price - second.price;
+      case 'Top rated first':
+        return second.rating - first.rating;
+      default:
+        return 0;
+    }
+  });
 
   const points: Point[] = filteredOffers.map((offer) => ({
     title: offer.id,
@@ -35,6 +50,10 @@ function MainPage(): JSX.Element {
 
   const handleCityChange = (city: City) => {
     dispatch(setActiveCity(city));
+  };
+
+  const handleSortModeChange = (mode: SortingMode) => {
+    setSortingMode(mode);
   };
 
   return (
@@ -87,33 +106,8 @@ function MainPage(): JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{filteredOffers.length} {getPlacesText(filteredOffers.length)} to stay in {activeCity.name}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-              Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select"/>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
-              <OfferList offers = {filteredOffers} onItemHover={setSelectedOfferId}/>
+              <SortingModes onModeChange={handleSortModeChange} />
+              <OfferList offers = {sortedOffers} onItemHover={setSelectedOfferId}/>
             </section>
             <div className="cities__right-section">
               <section className="cities__map map">
---
+++ src/types/sorting-mode.ts
@@ -0,0 +1,3 @@
+﻿import {SORTING_MODE} from '../const.tsx';
+
+export type SortingMode = SORTING_MODE.Popular | SORTING_MODE.Decreasing | SORTING_MODE.Rating | SORTING_MODE.Increasing;
